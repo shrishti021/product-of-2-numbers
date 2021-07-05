74b0599f6cb1c5af1796914de77b3e18ba06a518
shrishti021 <86967727+shrishti021@users.noreply.github.com>
2021-07-05 19:27:22 +0530

    This file prints product of two numbers

---
+++ product.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=0,b=0;
+    int product=0;
+    cout<<"Enter two numbers : ";
+    cin>> a >> b;
+    product = (a*b);
+    cout<<"the product of " << a << " and " << b << " is " << product ;
+    
+    return 0;
+}
